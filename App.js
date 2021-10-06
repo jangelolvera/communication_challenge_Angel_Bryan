298fafc3a5fbdd3b34af29ae78a00b83a677288e
jangelolvera <70440610+jangelolvera@users.noreply.github.com>
2021-10-05 20:14:25 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,54 @@
-import React, { useState, useRef } from 'react'; 
+import React, { useState, useRef } from 'react';
 import './App.css';
 import formulario from './formulario.jpeg';
 
 function App() {
 
-  const [status, setStatus]= useState('');
+  const [status, setStatus] = useState('');
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
+
     event.preventDefault();
+
     let emptyVal;
-      for(let i=0; i<17; i++){
-        if(event.target[i].value===''){
-          emptyVal=true;
-        }
+    let dispArr = document.querySelectorAll(".hinput");
+
+    for (let i = 0; i < 17; i++) {
+
+      if (event.target[i].value === '') {
+
+        emptyVal = true;
+
       }
-    if(emptyVal===true){
-      alert("ingresa todos los campos!!");
+
+      dispArr[i].value = event.target[i].value;
+
     }
+
+    if (emptyVal === true) {
+
+      alert("ingresa todos los campos!!");
+
+    } else {
+
+      setStatus('complete');
+      
+      for(let i=0; i<17; i++){
+        event.target[i].readOnly=true;
+      }
+
+    }
+
+  }
+
+  function handleClick(event){
+    setStatus('');
+
+    let writeAgain= document.querySelectorAll(".datos");
+    for(let i=0; i<17; i++){
+      writeAgain[i].readOnly=false;
+    }
+
 
   }
 
@@ -25,72 +57,94 @@ function App() {
 
       <h2>Fill in the blanks</h2>
 
-      <form 
-        id="blanks" 
+      <form
+        id="blanks"
         onSubmit={handleSubmit}
-        value={status} >
-          
+        value={status} 
+        >
+
         <label htmlFor="recipient">Name of recipient: </label>
-        <input id="recipient" type="text" />
+        <input id="recipient" className="datos" type="text" />
 
         <label htmlFor="sender">Name of sender: </label>
-        <input id="sender" type="text" />
+        <input id="sender" className="datos" type="text" />
 
         <label htmlFor="relationship">Relationship: </label>
-        <input id="relationship" type="text" />
+        <input id="relationship" className="datos" type="text" />
 
         <label htmlFor="yourevent">Event: </label>
-        <input id="yourevent" type="text" />
+        <input id="yourevent" className="datos" type="text" />
 
         <label htmlFor="modifier">Modifier: </label>
-        <input id="modifier" type="text" />
+        <input id="modifier" className="datos" type="text" />
 
         <label htmlFor="emotion">Emotion: </label>
-        <input id="emotion" type="text" />
+        <input id="emotion" className="datos" type="text" />
 
         <label htmlFor="namesend">Name of sender: </label>
-        <input id="sender" type="text" />
+        <input id="sender" className="datos" type="text" />
 
         <label htmlFor="activity">Activity: </label>
-        <input id="activity" type="text" />
+        <input id="activity" className="datos" type="text" />
 
         <label htmlFor="secondact">Second activity: </label>
-        <input id="secondact" type="text" />
+        <input id="secondact" className="datos" type="text" />
 
         <label htmlFor="unplanned">Unplanned activity: </label>
-        <input id="unplanned" type="text" />
+        <input id="unplanned" className="datos" type="text" />
 
         <label htmlFor="weekday">Day of the week: </label>
-        <input id="weekday" type="text" />
+        <input id="weekday" className="datos" type="text" />
 
         <label htmlFor="month">Month: </label>
-        <input id="month" type="text" />
+        <input id="month" className="datos" type="text" />
 
         <label htmlFor="day">Day: </label>
-        <input id="day" type="text" />
+        <input id="day" className="datos" type="text" />
 
         <label htmlFor="year">Year: </label>
-        <input id="year" type="text" />
+        <input id="year" className="datos" type="text" />
 
         <label htmlFor="time">Start time: </label>
-        <input id="time" type="text" />
+        <input id="time" className="datos" type="text" />
 
         <label htmlFor="location">Location/ Address: </label>
-        <input id="location" type="text" />
+        <input id="location" className="datos" type="text" />
 
         <label htmlFor="addnote">Additional notes: </label>
-        <input id="addnote" type="text" />
+        <input id="addnote" className="datos" type="text" />
 
         <div id="buttons">
-          <button type="reset" id="resbtn">Reset</button>
+          <button type="reset" id="resbtn" onClick={handleClick}>Reset</button>
           <button type="submit" id="genbtn">Generate</button>
         </div>
 
       </form>
 
-      <div id="hoja">
+      <div id="disInfo">
+
+        <input id="hidden0" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden1" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden2" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden3" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden4" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden5" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden6" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden7" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden8" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden9" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden10" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden11" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden12" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden13" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden14" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden15" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+        <input id="hidden16" className="hinput" type="text" defaultValue={status.value} onSubmit={handleSubmit} />
+
 
       </div>
+
+      <img src={formulario}/>
     </div>
   );
 }
